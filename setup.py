ffb273b1051433686b57fa0f1b0f5a227ee2e352
lemony999 <lemony2533@gmail.com>
2019-12-17 11:16:26 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setup(name="unit-test-for-data-science",
                         "pytest-mpl==0.10",
                         "pytest-mock==1.11.2",
                         "scipy==1.3.1",
+                        "codecov==2.0.15"
                         ],
       )
